83cc01f5b6ccd5b355737097505c5b1c0a99d728
Seaker <seaker000@gmail.com>
2017-03-19 00:36:23 +0200

    Adafruit_INA219 Arduino library ported

--- ChargingController/ChargingController.cpp
+++ ChargingController/ChargingController.cpp
@@ -1,19 +1,43 @@
 // Charging Contorller Main Source
 
 #include "ChargingController.h"
+#include "INA219/INA219.h"
 #include <iostream>
+#include <unistd.h>
+
 
 int main(void)
 {
 
 	std::cout << "[ChargingController] Started" << std::endl;
-	
-	BlackLib::BlackGPIO led1(BlackLib::GPIO_24, BlackLib::output, BlackLib::FastMode);
-	led1.setValue(BlackLib::high);
 
-	while (1)
+	INA219 ina219;
+
+	ina219.begin();
+	ina219.setCalibration_16V_400mA();
+
+	while(true)
 	{
 		// Charging algorithm
+
+		float shuntvoltage = 0;
+		float busvoltage = 0;
+		float current_mA = 0;
+		float loadvoltage = 0;
+
+		shuntvoltage = ina219.getShuntVoltage_mV();
+		busvoltage = ina219.getBusVoltage_V();
+		current_mA = ina219.getCurrent_mA();
+		loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+
+
+		std::cout << std::endl << "bus: " << busvoltage << std::endl;
+		std::cout << "shuntvoltage: " << shuntvoltage << std::endl;
+		std::cout << "current_mA: " << current_mA * 0.001 << std::endl;
+		std::cout << "loadvoltage: " << loadvoltage << std::endl;
+
+		usleep(2000);
 
 	}
 
